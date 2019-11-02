100eadcda6b5472996ad14e34d81cd6d295c8727
Xiaochen Wang <xiaochen@sensorscall.com>
2019-11-02 16:33:25 -0400

    fix the sounce sound offset

--- src/Main/STUN.java
+++ src/Main/STUN.java
@@ -146,7 +146,7 @@ public class STUN implements Runnable {
                             if (isPhoneOnline) {
                                 // forward the sound packet to device
                                 DebugMessage.log(TAG, sourcePacket.getLength() + "");
-                                sourceSoundPacket = new DatagramPacket(sourceBuffer, 5, sourceBuffer.length - 4, phoneAdd.getIpAddress(), phoneAdd.getPort());
+                                sourceSoundPacket = new DatagramPacket(sourceBuffer, 4, sourceBuffer.length - 4, phoneAdd.getIpAddress(), phoneAdd.getPort());
                                 communicationPort.send(sourceSoundPacket);
                             }
                         }
